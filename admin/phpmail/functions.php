2f96ebe8295f1d8c6f303c5978ac90e956b7a695
theaakashpradhan <102057057+theaakashpradhan@users.noreply.github.com>
2023-01-20 11:33:21 +0545

    Add files via upload

---
+++ admin/phpmail/functions.php
@@ -0,0 +1,49 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+
+function sendMail($message, $rec) {
+    require_once "PHPMailer.php";
+    require_once "Exception.php";
+    require_once "SMTP.php";
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'store.homeappliance@gmail.com';                     //SMTP username
+    $mail->Password   = 'Home@123';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('store.homeappliance@gmail.com', 'Kings Store');
+    $mail->addAddress($_POST["email"]);     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'OTP for Admin Login';
+    $mail->Body    = "OTP for Admin Login<br>OTP for login is: <b>Bhoot</b> <br><p style='color:green;'> This is the otp for your login. Do not share OTP with anybody.</p>";
+    $mail->AltBody = 'This is the otp for your login. Do not share OTP with anybody.';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+}
